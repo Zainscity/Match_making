77e8a8fd8c317dd56f9436d9da8e083912cd7d96
Zain ul Abideen <mzainh06@gmail.com>
2025-07-15 16:07:29 +0500

    Commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 from agents import AsyncOpenAI, OpenAIChatCompletionsModel, Agent, Runner, function_tool, RunConfig
-from ddgs import DDGS  # ✅ DuckDuckGo search tool
+from duckduckgo_search import DDGS  # ✅ DuckDuckGo search tool
 from twilio.rest import Client
 
 load_dotenv()
